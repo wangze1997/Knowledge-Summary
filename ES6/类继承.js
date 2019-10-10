8305849ca2a89a913a7212d65f4f4f90658547cd
wangze1997 <1826284334@qq.com>
2019-10-10 11:44:45 +0800

    es6继承

--- ES6/类继承.js
+++ ES6/类继承.js
@@ -3,8 +3,14 @@ class Parent{
         this.son()
         this.father()
         console.log(this.arr)
+        Son.fooSon();
+        Son.fooFather();
+        Son.fooParent();
     }
     arr = [4,3,2,1]
+    static fooParent() {
+        console.log('static fooParent')
+    }
 }
 class Father extends Parent{
     // 在父类的构造方法中。。。。
@@ -16,9 +22,13 @@ class Father extends Parent{
         console.log('father')
     }
     arr = [1,2,3,4]
+    static fooFather() {
+        console.log('static fooFather')
+    }
 }
 // 属性和方法加到this上的时间不一致。一个是父类构造之前，一个是父类构造之后。
 // 并且属性是直接加入到构造出的对象上的，不会出现在原型上
+// 静态方法与方法加载一致
 class Son extends Father{
     constructor(){ 
        super(); // super不写不行。
@@ -26,7 +36,9 @@ class Son extends Father{
     son() {
         console.log('son')
     }
-    name = "caiwenji"
+    static fooSon() {
+        console.log('static fooSon')
+    }
 }
 
 let son = new Son();
